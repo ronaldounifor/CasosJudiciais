990ba9bfba1e04a41edff434913837d905a37473
Ronaldo <rxp152830@utd.edu>
2024-05-13 14:18:45 -0300

    Trilha 2 - Spring Security

--- README.md
+++ README.md
@@ -1,4 +1,11 @@
-Project Title: Casos Judiciais.
-Description: Este projeto pode ser utilizado como material de apoio no aprendizado da Capacitação Java Avançado.
-License: GNU General Public License (GPL).
-Credits: Projeto desenvolvido pelo prof. Ronaldo Gonçalves Junior.
+# Project Title
+Casos Judiciais
+
+# Description
+Este projeto pode ser utilizado como material de apoio no aprendizado da Capacitação Java Avançado.
+
+# License
+GNU General Public License (GPL).
+
+# Credits
+Projeto desenvolvido pelo prof. Ronaldo Gonçalves Junior.
---
+++ src/main/java/br/cnj/casosjudiciais/config/WebSecurityConfig.java
@@ -0,0 +1,34 @@
+package br.cnj.casosjudiciais.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+
+@Configuration
+@EnableWebSecurity
+public class WebSecurityConfig { 
+    @Autowired
+    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+        auth.inMemoryAuthentication()
+        .withUser("admin").roles("ADMIN").password("{noop}123");
+    }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        return http.authorizeHttpRequests(request -> request.
+                requestMatchers(new AntPathRequestMatcher("/api/casos/**"))
+                .hasRole("ADMIN")
+                .anyRequest()
+                .authenticated())
+                .httpBasic(Customizer.withDefaults())
+                .csrf(AbstractHttpConfigurer::disable)
+                .build();
+    }
+}
